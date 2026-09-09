4f3e145bbd655a3086ecb64ea64cb80c4be84ccf
jheyliandro <jheyliandro@github.com>
2026-09-09 19:32:20 -0300

    feat: cria entidade de usuário com TypeORM

---
+++ src/entities/User.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  ATTENDANT = 'attendant',
+}
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ type: 'varchar', length: 120 })
+  name!: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  email!: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password!: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.ADMIN,
+  })
+  role!: UserRole;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+}
